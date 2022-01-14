488ece4281bc847cd26a7b73d6911b9c34a4f8e0
DeadorDev <inkohnito4@gmail.com>
2022-01-14 18:22:46 +0200

    Added: - AdminController - DB MySQL - FlyWay - button "Add Category" is working Updated version

---
+++ src/main/java/com/plocky/deador/model/Category.java
@@ -0,0 +1,20 @@
+package com.plocky.deador.model;
+
+
+import lombok.Data;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "category_id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+}
---
+++ src/main/java/com/plocky/deador/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.plocky.deador.repository;
+
+import com.plocky.deador.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+}
