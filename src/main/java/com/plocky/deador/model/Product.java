8846d4ce5ee9419cb092e94e9499f2529769cadc
DeadorDev <inkohnito4@gmail.com>
2022-01-16 14:09:31 +0200

    Added: - button "Add product" in "Products" is working

---
+++ src/main/java/com/plocky/deador/model/Product.java
@@ -0,0 +1,25 @@
+package com.plocky.deador.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private long id;
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", referencedColumnName = "category_id")
+    private Category category;
+    private double price;
+    private double weight;
+    private String description;
+    private String imageName;
+}
