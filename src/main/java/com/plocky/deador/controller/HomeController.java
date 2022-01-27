84591924af3099dd7780001ba7b6a29bc383ffdd
DeadorDev <inkohnito4@gmail.com>
2022-01-27 14:18:21 +0200

    Added: - buttons "Delete" and "Update" in "Products" is working

---
+++ src/main/java/com/plocky/deador/controller/HomeController.java
@@ -0,0 +1,30 @@
+package com.plocky.deador.controller;
+
+
+import com.plocky.deador.service.CategoryService;
+import com.plocky.deador.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+    @Autowired
+    CategoryService categoryService;
+    @Autowired
+    ProductService productService;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
