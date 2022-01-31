ea309508ea1fba159042e7233fd3d8977e6b19df
DeadorDev <inkohnito4@gmail.com>
2022-01-31 19:28:46 +0200

    Added: - registration is working

--- src/main/java/com/plocky/deador/controller/HomeController.java
+++ src/main/java/com/plocky/deador/controller/HomeController.java
@@ -5,6 +5,9 @@ import com.plocky.deador.service.CategoryService;
 import com.plocky.deador.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
@@ -13,18 +16,29 @@ public class HomeController {
     @Autowired
     ProductService productService;
 
+    @GetMapping({"/", "/home"})
+    public String home(Model model) {
+        return "/index";
+    }
 
+    @GetMapping("/shop")
+    public String shop(Model model) {
+        model.addAttribute("categories", categoryService.getAllCategory());
+        model.addAttribute("products", productService.getAllProduct());
+        return "/shop";
+    }
 
+    @GetMapping("/shop/category/{id}")
+    public String shopByCategory(@PathVariable int id, Model model) {
+        model.addAttribute("categories", categoryService.getAllCategory());
+        model.addAttribute("products", productService.getAllProductsByCategoryId(id));
+        return "/shop";
+    }
 
-
-
-
-
-
-
-
-
-
-
+    @GetMapping("/shop/viewproduct/{id}")
+    public String viewProduct(@PathVariable int id, Model model) {
+        model.addAttribute("product", productService.getProductById(id).get());
+        return "/viewProduct";
+    }
 
 }
