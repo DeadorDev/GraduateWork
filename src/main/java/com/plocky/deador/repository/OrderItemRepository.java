5311e3689a4fca756c144c1cfed0b774d2a7bc83
DeadorDev <inkohnito4@gmail.com>
2022-03-17 10:59:34 +0200

    Added: - "/admin/orders" - "/admin/orders/orderMore" - "/admin/orders/orderMore/update"

--- src/main/java/com/plocky/deador/repository/OrderItemRepository.java
+++ src/main/java/com/plocky/deador/repository/OrderItemRepository.java
@@ -1,6 +1,5 @@
 package com.plocky.deador.repository;
 
-import com.plocky.deador.model.Order;
 import com.plocky.deador.model.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +7,6 @@ import java.util.List;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
     public List<OrderItem> findAll();
+
+    public List<OrderItem> findAllByOrderId(Integer id);
 }
--- src/main/java/com/plocky/deador/repository/OrderRepository.java
+++ src/main/java/com/plocky/deador/repository/OrderRepository.java
@@ -4,9 +4,14 @@ import com.plocky.deador.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Integer> {
 
     List<Order> findAllByEmail(String userEmail);
+
+    List<Order> findAll();
+
+    Optional<Order> findById(Integer id);
 
 }
